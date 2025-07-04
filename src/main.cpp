bbc57557074d0b581ecc2c972791f57670893086
tianxingjian2791 <2791617524@qq.com>
2025-07-04 18:39:04 +0200

    Generate Stokes dataset

--- src/main.cpp
+++ src/main.cpp
@@ -20,74 +20,9 @@ int main(int argc, char* argv[])
         {
             using namespace AMGDiffusion;
 
-            std::ofstream file("./datasets/train/raw/train_.csv");
+            std::ofstream file("./datasets/train/raw/train.csv");
+            generate_dataset(file);
             
-            // 参数范围 (9600个样本 = 4模式 × 12ε × 25θ × 8网格)
-            const std::array<DiffusionPattern, 4> patterns = {{
-                DiffusionPattern::vertical_stripes,
-                DiffusionPattern::vertical_stripes2,
-                DiffusionPattern::checkerboard2,
-                DiffusionPattern::checkerboard
-            }};
-            
-            const std::vector<double> epsilon_values = 
-                // Solver<2>::linspace(0.0, 9.5, 40); // dataset2
-                Solver<2>::linspace(0.0, 9.5, 12); // dataset1
-            
-            const std::vector<double> theta_values = 
-                // Solver<2>::linspace(0.02, 0.9, 45); // dataset2
-                Solver<2>::linspace(0.02, 0.9, 25); // dataset1
-            
-            const std::vector<unsigned int> refinements = 
-                // {3, 4, 5, 6}; // dataset2
-                // {8};
-                // {7}; // dataset1: test
-                {3, 4, 5, 6}; // dataset1: train
-            
-            unsigned int sample_index = 0;
-            
-            // 遍历所有参数组合
-            for (auto pattern : patterns) {
-
-                // set_pattern(pattern);
-                switch (pattern)
-                {
-                case DiffusionPattern::vertical_stripes: std::cout<<"vertical_stripes"<<std::endl; break;
-                case DiffusionPattern::vertical_stripes2: std::cout<<"vertical_stripes2"<<std::endl; break;
-                case DiffusionPattern::checkerboard2: std::cout<<"checkerboard2"<<std::endl; break;
-                case DiffusionPattern::checkerboard: std::cout<<"checkerboard"<<std::endl; break;
-                }
-                
-                
-                for (double epsilon : epsilon_values) {
-                // set_epsilon(epsilon);
-                
-                for (unsigned int refinement : refinements) {
-                    // set_refinement(refinement);
-                    
-                    // 重新生成网格和系统
-                    // triangulation.clear();
-                    // make_grid();
-                    // setup_system();
-                    // assemble_system();
-                    
-                    for (double theta : theta_values) {
-                    Solver<2> solver(pattern, epsilon, refinement);
-                    // std::cout<<"theta to be set: "<<theta<<std::endl;
-                    solver.set_theta(theta);
-                    solver.run(file);
-                    sample_index++;
-                    
-                    // if (sample_index % 100 == 0) {
-                    // if (sample_index) {
-                    std::cout << "Generated " << sample_index << "/28800 samples" << std::endl;
-                    // }
-                    }
-                }
-                }
-            }
-            std::cout << "Dataset generation complete. Total samples: " << sample_index << std::endl;
-
         }
         else if (std::strcmp(argv[1], "E") == 0)
         {
@@ -101,9 +36,12 @@ int main(int argc, char* argv[])
         {
             using namespace AMGStokes;
             std::cout << "Generating the dataset of Stokes equations" << std::endl;
-            // generate_stokes_dataset();      
-            StokesProblem<2> stokes_problem(2);
-            stokes_problem.run();
+
+            std::ofstream file("./datasets/train/raw/train3.csv");
+            // generate_dataset(file);    
+
+            StokesProblem<2> stokes_problem(2, 0.1, 0);
+            stokes_problem.run(file);
         }
         else
         {
