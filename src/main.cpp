4e8f7cf1b20bc679166f5869cf4d01b3a248fde6
tianxingjian2791 <2791617524@qq.com>
2025-07-07 12:48:30 +0200

    add test option for generate_dataset function

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,23 @@ int main(int argc, char* argv[])
         // 初始化 MPI（在串行模式下也可以安全调用）
         Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
         
-        if (argc < 2){
-            std::cerr << "Usage: " << argv[0] << " [D|S]\n";
+        if (argc < 3){
+            std::cerr << "Usage: " << argv[0] << "The first argument must be [D|S|E] and the second argument must be [train|test]\n";
             return 1;
         }
+
 
         if (std::strcmp(argv[1], "D") == 0)
         {
             using namespace AMGDiffusion;
             std::cout << "Generating the dataset of Diffusion equations" << std::endl;
 
-            std::ofstream file("./datasets/train/raw/train.csv");
-            generate_dataset(file);
+            std::ofstream file("./datasets/test/raw/test1.csv");
+            if (std::string(argv[2]) == "train")
+            {
+                std::ofstream file("./datasets/train/raw/train1.csv");
+            }
+            generate_dataset(file, std::string(argv[2]));
             
         }
         else if (std::strcmp(argv[1], "E") == 0)
@@ -30,8 +35,12 @@ int main(int argc, char* argv[])
             using namespace AMGElastic;
             std::cout << "Generating the dataset of Elastic equations" << std::endl;
 
-            std::ofstream file("./datasets/train/raw/train2.csv");
-            generate_dataset(file);
+            std::ofstream file("./datasets/test/raw/test2.csv");
+            if (std::string(argv[2]) == "train")
+            {
+                std::ofstream file("./datasets/train/raw/train2.csv");
+            }
+            generate_dataset(file, std::string(argv[2]));
             
             // ElasticProblem<2> elastic_problem;
             // elastic_problem.run();
@@ -41,8 +50,12 @@ int main(int argc, char* argv[])
             using namespace AMGStokes;
             std::cout << "Generating the dataset of Stokes equations" << std::endl;
 
-            std::ofstream file("./datasets/train/raw/train3.csv");
-            generate_dataset(file);    
+            std::ofstream file("./datasets/test/raw/test3.csv");
+            if (std::string(argv[2]) == "train")
+            {
+                std::ofstream file("./datasets/train/raw/train3.csv");
+            }
+            generate_dataset(file, std::string(argv[2]));  
 
             // StokesProblem<2> stokes_problem(2, 0.1, 0);
             // stokes_problem.run(file);
@@ -50,7 +63,7 @@ int main(int argc, char* argv[])
         else
         {
             std::cerr << "Unknown option: " << argv[1] << "\n"
-                  << "Usage: " << argv[0] << " [D|S]\n";
+                  << "Usage: " << argv[0] << " [D|S|E]\n";
             return 1;
         }
     }
